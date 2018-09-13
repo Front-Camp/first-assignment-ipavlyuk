6e4ad8ecca8d4e91237de89e01d2aa484828424f
Iryna Pavlyuk <32683206+ipavlyuk@users.noreply.github.com>
2018-09-13 20:29:36 +0300

    Update index.js

--- src/sum/index.js
+++ src/sum/index.js
@@ -10,7 +10,21 @@
 * sum(-1, 13); // 12
 */
 const sum = (a, b) => {
-  /* your logic here...*/
+   let newArr = [];
+    let sum = 0;
+    for (let i = 0, b = 1 ; i < arr.length; i++, b++) {
+        newArr.push(arr[i]);
+        sum += arr[i];
+        if ( b === 2 ) {
+            if (sum === base) {
+                return newArr;
+            } else {
+                b = 1;
+                newArr = [];
+                i = 1;
+            }
+        }
+    }
 };
 
 export default sum;
